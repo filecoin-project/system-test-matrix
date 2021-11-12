8a887c96b659d79bbf7d243db4e3784effe1d087
aleksaToljic <aleksa.toljic@gmail.com>
2021-11-12 22:06:57 +0100

    chore: Initial version of matrix map

--- frontend/apps/web/src/pages/repository-details/index.tsx
+++ frontend/apps/web/src/pages/repository-details/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import {
+  PageLayout,
+  StackLayout,
+  Text,
+  TreeMap,
+  usePageLayout,
+} from '@filecoin/ui'
+
+import { PageContainer } from '@/containers/PageContainer'
 
 const RepositoryDetails = () => {
-  return <div></div>
+  const {
+    state: { model },
+  } = PageContainer.useContainer()
+  const systems = model.getAllSystems()
+
+  const pageLayout = usePageLayout({
+    header: (
+      <PageLayout.Header>
+        <StackLayout>
+          <Text type="heading 5">Systems</Text>
+        </StackLayout>
+      </PageLayout.Header>
+    ),
+    footer: <PageLayout.Footer />,
+  })
+  return (
+    <PageLayout {...pageLayout}>
+      <PageLayout.Section>
+        <TreeMap data={systems[0].subsystems[1].tests} onClick={() => null} />
+      </PageLayout.Section>
+    </PageLayout>
+  )
 }
 
 export default RepositoryDetails

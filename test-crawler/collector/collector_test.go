47ee8ca8a828940fd3d351f2083c384d345572f6
bokimilinkovic <boki1997@gmail.com>
2022-11-28 11:41:43 +0100

    Use tempDir to create temp files in tests

--- test-crawler/collector/collector_test.go
+++ test-crawler/collector/collector_test.go
@@ -3,6 +3,7 @@ package collector
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 
@@ -23,20 +24,23 @@ func TestListGoFilesInFolder(t *testing.T) {
 		systemName := splitedFolderName[len(splitedFolderName)-1]
 		assert.Equal(t, system, systemName)
 		assert.Equal(t, 2, len(files))
+		os.Remove(tempDir)
 	})
 
-	t.Run("Read golang files from mocks/config directory", func(t *testing.T) {
-		system, files, err := ListGoFilesInFolder("../mocks/config", nil)
+	t.Run("Read golang files excluding everything else", func(t *testing.T) {
+		tempDir := t.TempDir()
+		err := ioutil.WriteFile(fmt.Sprintf("%s/a.go", tempDir), []byte("package main func main(){a:=1}"), 0644)
+		err = ioutil.WriteFile(fmt.Sprintf("%s/b.go", tempDir), []byte("package main func main(){b:=2}"), 0644)
+		err = ioutil.WriteFile(fmt.Sprintf("%s/b_test.go", tempDir), []byte("package main_test"), 0644)
+		err = ioutil.WriteFile(fmt.Sprintf("%s/.env", tempDir), []byte("PORT=8080"), 0644)
 		assert.NoError(t, err)
-		assert.Equal(t, system, "config")
+
+		system, files, err := ListGoFilesInFolder(tempDir, nil)
+		assert.NoError(t, err)
+		splitedFolderName := strings.Split(tempDir, "/")
+		systemName := splitedFolderName[len(splitedFolderName)-1]
+		assert.Equal(t, system, systemName)
 		assert.Equal(t, 2, len(files))
+		os.Remove(tempDir)
 	})
-
-	t.Run("Read golang files from mocks directory", func(t *testing.T) {
-		system, files, err := ListGoFilesInFolder("../mocks", nil)
-		assert.NoError(t, err)
-		assert.Equal(t, system, "mocks")
-		assert.Equal(t, 4, len(files))
-	})
-
 }

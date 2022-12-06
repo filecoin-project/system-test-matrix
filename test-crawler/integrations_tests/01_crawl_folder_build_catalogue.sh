71c522dd3a548d72efd48425ffebb352b7ea7e1c
bokimilinkovic <boki1997@gmail.com>
2022-12-06 14:25:27 +0100

    Remove granting access to files, and unnecesarry loggings in test

--- test-crawler/integrations_tests/01_crawl_folder_build_catalogue.sh
+++ test-crawler/integrations_tests/01_crawl_folder_build_catalogue.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 go run . -crawl=src
-find "outputs" -type f -exec echo Found file {} \;
 # for FILE in "outputs/*"; do echo $FILE | cat $FILE; done
 if [ -z "$(ls -A outputs)" ]; then
-   echo "Empty"
+   echo "No expected files in output directory"
    exit 1
-else
-   echo "Not Empty"
 fi
